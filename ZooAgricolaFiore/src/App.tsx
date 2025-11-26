183c605003e73771e8099ed22d04b11b35a910d8
andrei <andreipiticas1805@gmail.com>
2025-11-26 11:22:07 +0100

    add basename for github pages

--- ZooAgricolaFiore/src/App.tsx
+++ ZooAgricolaFiore/src/App.tsx
@@ -51,7 +51,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/sito_zooagricolafiore">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
