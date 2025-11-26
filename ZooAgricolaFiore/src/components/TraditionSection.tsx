14bda5fb9385d1df03d0a264306473d9182ef5d1
andrei <andreipiticas1805@gmail.com>
2025-11-26 14:13:53 +0100

    sistemazione video

--- ZooAgricolaFiore/src/components/TraditionSection.tsx
+++ ZooAgricolaFiore/src/components/TraditionSection.tsx
@@ -74,7 +74,7 @@ const TraditionSection = () => {
                 className="w-full h-auto object-cover"
                 style={{ maxHeight: '500px' }}
               >
-                <source src="/sito_zooagricolafiore/video_zooagricola.mp4" type="video/mp4" />
+                <source src={videoZooagricola} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
             </div>
