6d3c3895615355a711a70f88c80b5cd8e6da9c74
andrei <andreipiticas1805@gmail.com>
2025-11-26 11:02:00 +0100

    setup github pages deployment

--- ZooAgricolaFiore/vite.config.ts
+++ ZooAgricolaFiore/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/sito_zooagricolafiore/",
   server: {
     host: "::",
     port: 8080,
